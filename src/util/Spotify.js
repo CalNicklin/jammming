ad6c023843f3765bb28a1e0f65cffa9ee6e27029
Cal <calpnicklin@gmail.com>
2022-06-06 11:08:16 +0100

    adds spotify.js

---
+++ src/util/Spotify.js
@@ -0,0 +1,50 @@
+let userAccessToken;
+const clientId = b533500775164346aeadd6758b2fbb2c;
+const redirectUri = http://localhost:3000/;
+
+const Spotify = {
+    getAccessToken() {
+        if (userAccessToken) {
+            return userAccessToken;
+        }
+
+        // check for access token match 
+        const userAccessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (userAccessTokenMatch && expiresInMatch) {
+            userAccessToken = userAccessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            // This clears the parameters, allowing us to get new UAT when expired
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return userAccessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl; 
+        };
+    },
+
+    search(term) {
+        const userAccessToken = Spotify.getAccessToken();  
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, 
+        {
+            headers: {Authorization: `Bearer ${userAccessToken}`}
+        }).then (response => {
+            return response.json(); 
+        }).then (jsonResponse => {
+            if (!jsonResponse.tracks) {
+                return [];
+            }
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+        })
+    }
+}
+
+export default Spotify;
