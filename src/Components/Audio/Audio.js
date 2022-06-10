555dd4afb5f00d1347971a4960b596fa6a4c31b9
Cal <calpnicklin@gmail.com>
2022-06-10 10:00:25 +0100

    adds audio sample feature

--- src/Components/Audio/Audio.js
+++ src/Components/Audio/Audio.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 class AudioPlayer extends React.Component {
     render() {
+        const isSampleAudio = this.props.audio;
+        let player;
+
+        if (isSampleAudio !== null) {
+            player = <audio src={isSampleAudio} controls />
+        } else {
+            player = <h3>Sample audio unavaiable</h3>
+        }
+
         return (
             <div className='audio'>
-                <audio src={this.props.audio} controls />
+                {player}
             </div>
         );
     }
